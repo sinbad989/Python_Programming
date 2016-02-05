5884cfdeb1d31181fba8a38e47436a1c5c91e496
Anthony Val  C <anthonysinbad@gmail.com>
2016-02-05 15:34:31 +0800

    Unsolved
    next day nalang

--- CodeFight/day2.py
+++ CodeFight/day2.py
@@ -7,14 +7,30 @@ mat = [[4,5,3,-4,3,8],
  [1,4,7,8,9,11], 
  [1,3,10,5,5,1], 
  [12,7,3,4,4,3]]
-x = dict()
+
+x = list()
+d = dict()
 
 for elem in mat:
     for j in elem:
-        x[j] = x[j]+ x.get(j,1)
+        x.append(j)
+
+for c in x:
+    if c not in d:
+        d[c] = 1
+    else:
+        d[c] += 1
+        
+num = d.keys()
+num.sort()
 
 #if zero exist
-
+lnum = 0
+for k in range(len(num)-1):
+    if num[k+1] > num[k]:
+        lnum = num[k]
+    if lnum - num[len(num)] == 1:
+        lnum = num[len(num)]+1
 #if middle number exist
 
 #if middle number is missing 
